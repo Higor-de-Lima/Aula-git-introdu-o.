2736bae78f65694e6ad96da73b761e5ad24fb16b
Higor <higor130410@gmail.com>
2024-02-01 18:42:20 +0000

    mensagem

---
+++ Ex1.py
@@ -0,0 +1,8 @@
+login = str(input('Digite um Login:'))
+senha = str(input('Digite uma senha:'))
+
+if login == senha:
+    print("senha e login Iguais")
+else:
+    print("Senha e Login Diferentes")
+    
